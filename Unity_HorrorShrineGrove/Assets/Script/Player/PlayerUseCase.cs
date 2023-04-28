b3fe380805842c6c3afed7940d71e1db1ea6bc57
Rsueyoshi1008 <r.sueyosi.humanghm@gmail.com>
2023-04-28 18:03:00 +0900

    add カメラ移動、キャラの移動実装途中

---
+++ Unity_HorrorShrineGrove/Assets/Script/Player/PlayerUseCase.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Script.Player
+{
+    public class PlayerUseCase
+    {
+        
+        public void Move(float horizontalInput , float verticalInput, float movespeed,Transform moveObject)
+        {
+           //ベクトルに変換する
+            Vector3 movement = new Vector3(horizontalInput,1.0f,verticalInput);
+
+            //移動ベクトルを正規化する
+            movement = movement.normalized;
+
+            //キャラの移動をする
+            moveObject.position = movement * movespeed * Time.deltaTime;
+            
+        }
+        
+    }
+
+}
